df00adb0693f7b0e138691232db686a7ae1b05d8
Rityelle0612 <rmvl1@aluno.ifal.edu.br>
2022-09-02 12:37:41 +0000

    atualizando

--- src/test/java/com/danielfireman/ifal/calcapi/IntegrationTest.java
+++ src/test/java/com/danielfireman/ifal/calcapi/IntegrationTest.java
@@ -31,7 +31,7 @@ public class IntegrationTest {
   @Test
   public void soma_erro(int serverPort) throws IOException {
     Request req = new Request.Builder()
-        .url("http://localhost:" + serverPort + "/soma/2a/2b")
+        .url("http://localhost:" + serverPort + "/soma/2a/2a")
         .build();
 
     try (Response rsp = client.newCall(req).execute()) {
