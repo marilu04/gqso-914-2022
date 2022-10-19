e5212c0601a68a68a10d2ed668a1b6ec336ba8f8
marilu04 <mlnl1@aluno.ifal.edu.br>
2022-10-19 14:17:43 +0000

    corrigindo erro

--- src/main/java/com/danielfireman/ifal/calcapi/Soma.java
+++ src/main/java/com/danielfireman/ifal/calcapi/Soma.java
@@ -9,7 +9,7 @@ public class Soma {
     @GET
     public double calculaSoma(@PathParam double num, @PathParam double num2) {
         try {
-          return num;
+          return num+num2;
         } catch (NumberFormatException nfe) {
           throw new BadRequestException("Parâmetros inválidos: " + num + " " +num2);
         }
